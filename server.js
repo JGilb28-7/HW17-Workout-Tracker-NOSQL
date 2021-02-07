a79ebe9e6cc0e42355506ccc0f8fb57bf9c856e6
JGilbe28-7 <jonathan.gilbert@mac.com>
2021-02-06 23:55:24 -0500

    server.js and Modules

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+  { useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  }
+);
+
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
+// Start the server*/
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
